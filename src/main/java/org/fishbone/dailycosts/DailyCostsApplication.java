d796a09c5be35505ef9b122c2bd24258ac91bc89
Koval <hazardsales@gmail.com>
2022-09-17 16:16:16 +0200

    Init commit

---
+++ src/main/java/org/fishbone/dailycosts/DailyCostsApplication.java
@@ -0,0 +1,13 @@
+package org.fishbone.dailycosts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyCostsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyCostsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/fishbone/dailycosts/DailyCostsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.fishbone.dailycosts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyCostsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
